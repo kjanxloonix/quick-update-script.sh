c0e26822ea770719a6312f703ce5d0821908a07d
kjanxlinux <kja@onet.eu>
2021-09-10 22:17:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# quick-update-script.sh
---
+++ quick-upgrade-script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+green="\033[1;32m"
+red="\033[1;31m"
+clean="\033[0m"
+
+comm=( 'apt-get install -f'
+	'apt-get upgrade'
+	'apt-get upgrade'
+	'apt-get dist-upgrade'
+	'apt-get autoremove'
+	'apt-get autoclean'
+	'snap refresh')
+
+for i in "${comm[@]}"; do
+	printf "\n${green} $i${clean}\n"
+	$i
+	int_code=$?
+	if [[ $int_code != 0 ]]; then
+		printf "\n==>${red}Interruption: ErrorCode:${clean} $int_code\n\n"
+		exit $int_code
+	fi
+done
+
+printf "\n==>${red}Finished updating succesfully.${clean}\n\n"
+exit 0
